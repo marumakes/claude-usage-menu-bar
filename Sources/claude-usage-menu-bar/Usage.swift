d0b83f249d3e4abde10b0f985f213e36af0ef65e
Maru <238571780+marumakes@users.noreply.github.com>
2026-07-13 17:54:20 +0900

    feat: get usage data from Claude CLI

---
+++ Sources/claude-usage-menu-bar/Usage.swift
@@ -0,0 +1,45 @@
+import Foundation
+
+struct Usage {
+    let sessionPercentage: Int
+    let weeklyPercentage: Int
+    let sessionResetDate: Date
+    let weeklyResetDate: Date
+    let lastUpdated: Date
+}
+
+enum ClaudeError: Error {
+    case invalidData
+    case processFailed
+    case commandNotFound
+    case unknown
+}
+
+// runs a terminal command to access to the Claude CLI, and returns the usage data
+func runClaudeUsage() throws -> String {
+    // create process
+    let task = Process()
+    let outputPipe = Pipe()
+    let errorPipe = Pipe()
+    
+    // send anything the process prints to stdout into this pipe instead of the terminal
+    task.standardOutput = outputPipe
+    task.standardError = errorPipe
+
+    // call claude cli with /usage arg
+    task.arguments = ["-p", "/usage"]
+    task.executableURL = URL(fileURLWithPath: "/opt/homebrew/bin/claude")
+
+    try task.run()
+    task.waitUntilExit()
+    
+    let data = outputPipe.fileHandleForReading.readDataToEndOfFile()
+
+    guard let output = String(data: data, encoding: .utf8) else {
+        throw ClaudeError.invalidData
+    }
+    
+    // return the result
+    return output        
+}
+
--- Sources/claude-usage-menu-bar/claude_usage_menu_bar.swift
+++ Sources/claude-usage-menu-bar/claude_usage_menu_bar.swift
@@ -5,5 +5,11 @@
 struct claude_usage_menu_bar {
     static func main() {
         print("Hello, world!")
+        do {
+            let output = try runClaudeUsage()
+            print(output)
+        } catch {
+            print("invalid data error?")
+        }
     }
 }
